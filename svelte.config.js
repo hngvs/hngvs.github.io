0d70a6d8d87854d37d836e053e94e3ceb29265c9
Bret Palsson <bretep@gmail.com>
2022-07-03 20:16:46 -0700

    Add preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 const dev = "production" === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+    preprocess: preprocess(),
     kit: {
         adapter: adapter({
             // default options are shown
